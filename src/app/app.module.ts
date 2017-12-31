fb5d520b57770be76740bfccead4e0ac76157f49
aldunateadolfo <alkonvng@gmail.com>
2017-12-31 13:03:38 +0100

    asdsa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,25 +15,23 @@ import { RouterModule, Routes } from '@angular/router';
 // Components adolfo
 import { AppComponent } from './app.component';
 import { RegistroComponent } from './usuario/registro/registro.component';
-
-// import { MainComponent } from './main/main.component';
+import { MainComponent } from './main/main/main.component';
 import { NavBarComponent } from './main/nav-bar/nav-bar.component';
 import { LoginComponent } from './usuario/login/login.component';
 import { CreaMonitorComponent } from './monitor/crea-monitor/crea-monitor.component';
 import { Route } from './app.route';
 import { UsuarioService } from './usuario/service/usuario.service';
-// import { TokenInterceptor } from './usuario/service/token.interceptor';
 import { TokenService } from './usuario/service/token.service';
 import { BackendInterceptor } from './util/service/backend.interceptor';
 import { MonitorService } from './monitor/service/monitor.service';
 
 
 // Plugins
-import { MaterialAngularModule, MY_FORMATS } from './util/plugins/materialAngular';
+import { MaterialAngularModule, MY_FORMATS } from './util/plugins/material/materialAngular';
 import { MatNativeDateModule, MAT_DATE_FORMATS, MAT_DATE_LOCALE, DateAdapter } from '@angular/material';
 import 'hammerjs';
 import { MomentDateAdapter } from '@angular/material-moment-adapter';
-import { ImgUploadComponent } from './util/genericComponents/file-upload/img-upload.component';
+import { ImgUploadComponent } from './util/generic/img-upload/img-upload.component';
 import { EditarMonitorComponent } from './monitor/editar-monitor/editar-monitor.component';
 
 
