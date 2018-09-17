b8ea12bafecd09aba10d4c392b6410f7b9c39f4b
Max Aldunate <maxaldunate@gmail.com>
2018-09-17 09:45:20 +0200

    add cognito env variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,23 @@
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
-
 export const environment = {
-  production: false
+    production: false,
+
+    region: 'eu-west-1',
+
+    //identityPoolId: '',
+    userPoolId: 'eu-west-1_bG8wMzFAW',
+    clientId: '1gpm36pbf5c3mfaf2a9q1j6lt5',
+
+    //rekognitionBucket: '',
+    //albumName: "",
+    //bucketRegion: '',
+
+    ddbTableName: 'LoginTrail',
+
+    //cognito_idp_endpoint: '',
+    //cognito_identity_endpoint: '',
+    //sts_endpoint: '',
+    //dynamodb_endpoint: '',
+    //s3_endpoint: ''
+
 };
+
